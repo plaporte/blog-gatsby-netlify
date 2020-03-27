1abe0c5f5bf7dd347cb8b485c48070a1867da01c
Phil <plaporte@clubessential.com>
2020-03-26 19:24:32 -0500

    Fix excerpt

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap'
 
-const Post = (props) => (
-    <div className="col-lg-4 col-md-6 col-xs-12 p-3">
-        <Card>
-            <Card.Img variant="top" src={props.image}/>
-            <Card.Body>
-                <Card.Title dangerouslySetInnerHTML={{__html: props.title}} />
-                <Card.Text dangerouslySetInnerHTML={{ __html: props.excerpt }}/>
-            <Button variant="primary" href={props.readMore}>Read More</Button>
-            </Card.Body>
-        </Card>
-    </div>
-)
+const Post = props => (
+  <div className="col-lg-4 col-md-6 col-xs-12 p-3">
+    <Card>
+      <Card.Img variant="top" src={props.image} />
+      <Card.Body>
+        <Card.Title dangerouslySetInnerHTML={{ __html: props.title }} />
+        <Card.Text>
+          <div dangerouslySetInnerHTML={{ __html: props.excerpt }} />
+        </Card.Text>
+        <Button variant="primary" href={props.readMore}>
+          Read More
+        </Button>
+      </Card.Body>
+    </Card>
+  </div>
+);
 
 export default Post;
