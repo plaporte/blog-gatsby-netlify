1f9dba81ae836caaeafed8319fa12bcef7b06b50
Phil <plaporte@clubessential.com>
2020-03-26 19:30:33 -0500

    PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,15 +11,15 @@ module.exports = {
     description: `Self tracking my learning`,
     keywords: `gatsby, react, gatsbyjs, bootstrap, wordpress, learning project`,
     image: `/static/gatsby.jpg`,
-    url: `https://www.gatsbyjs.org/`,
+    url: `https://www.gatsbyjs.org/`
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/`,
-      },
+        path: `${__dirname}/src/`
+      }
     },
     { resolve: `gatsby-transformer-remark` },
     {
@@ -27,11 +27,23 @@ module.exports = {
       options: {
         baseUrl: `philslearningblog.wordpress.com`,
         protocol: `http`,
-        hostingWPCOM: true,
-      },
+        hostingWPCOM: true
+      }
     },
     `gatsby-plugin-react-helmet`,
-  ],
-}
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Phil's learning blog`,
+        short_name: `LearningBlog`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`
+      }
+    },
+    `gatsby-plugin-offline`,
+  ]
+};
 
 
